a2d1b1bdaf7fcc55bd5d444d87bf42aad89b5b00
CodeSampleH6AN <harissestan91@gmail.com>
2020-11-21 19:15:31 +0100

    up

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,5 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        document.addEventListener("backbutton", function (e) {
-            e.preventDefault();
-        }, false );
+document.addEventListener("backbutton", onBackKeyDown, false);
+
+function onBackKeyDown() {
+    // Handle the back button
 }
