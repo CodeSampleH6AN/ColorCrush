ff08d1d1a2ff5c5f316cdc956ea06cdc42daa26e
CodeSampleH6AN <harissestan91@gmail.com>
2020-11-23 17:03:20 +0100

    update

--- www/js/script3.js
+++ www/js/script3.js
@@ -18,29 +18,29 @@ window.onload = function() {
         var mkgbojeresult = (((mkgbriz - (mkgboje / ntest)) * tper)) / 100;
         var mkgbojeresultnt = (((mkgbriz - (mkgboje / ntest)) * tper)) / 10;
         document.getElementById('tperresult').innerHTML = "Trenutni procenat boje:&#160;" + tperresult.toFixed(2) + " %";
-        document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje:&#160;" + mkgbojeresult.toFixed(6) + " kg";
+        document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje za 1 brizganje:&#160;" + mkgbojeresult.toFixed(6) + " kg";
         document.getElementById('mkgbojeresultnt').innerHTML = "Tra&#382;ena masa boje na&#160;" + document.getElementById('ntest').value + "&#160;testiranja:&#160;" + mkgbojeresultnt.toFixed(6) + " kg";
 
         if (mkgbriz === "") {
             document.getElementById('tperresult').innerHTML = "Trenutni procenat boje:&#160;" + (mkgbriz - 0) + " %";
         }
         if (mkgbriz === "") {
-            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje:&#160;" + (mkgbriz - 0) + " kg";
+            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje za 1 brizganje:&#160;" + (mkgbriz - 0) + " kg";
         }
         if (mkgboje === "") {
             document.getElementById('tperresult').innerHTML = "Trenutni procenat boje:&#160;" + (mkgboje - 0) + " %";
         }
         if (mkgboje === "") {
-            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje:&#160;" + (mkgboje - 0) + " kg";
+            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje za 1 brizganje:&#160;" + (mkgboje - 0) + " kg";
         }
         if (ntest === "") {
             document.getElementById('tperresult').innerHTML = "Trenutni procenat boje:&#160;" + (ntest - 0) + " %";
         }
         if (ntest === "") {
-            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje:&#160;" + (ntest - 0) + " kg";
+            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje za 1 brizganje:&#160;" + (ntest - 0) + " kg";
         }
         if (tper === "") {
-            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje:&#160;" + (tper - 0) + " kg";
+            document.getElementById('mkgbojeresult').innerHTML = "Tra&#382;ena masa boje za 1 brizganje:&#160;" + (tper - 0) + " kg";
         }
         if (mkgbriz === "") {
             document.getElementById('mkgbojeresultnt').innerHTML = "Tra&#382;ena masa na&#160;" + (mkgbriz - 0) + "&#160;testiranja:&#160;" + (mkgbriz - 0) + " kg";
