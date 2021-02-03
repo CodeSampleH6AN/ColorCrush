99e07702a5877b0d8838f7dfa764f11ac68d8da3
CodeSampleH6AN <harissestan91@gmail.com>
2021-02-03 17:36:48 +0100

    update

--- www/js/no_character.js
+++ www/js/no_character.js
@@ -1,4 +1,7 @@
 $('html').bind('keypress', function (e) {
+    if (e.charCode == 43) {
+        return false;
+    }
     if (e.charCode == 44) {
         return false;
     }
@@ -6,6 +9,15 @@ $('html').bind('keypress', function (e) {
         return false;
     }
     if (e.charCode == 32) {
+        return false;
+    }
+    if (e.charCode == 173) {
+        return false;
+    }
+    if (e.charCode == 188) {
+        return false;
+    }
+    if (e.charCode == 189) {
         return false;
     }
 });
